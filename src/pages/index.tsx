0ed1f6e6d9af7b62e5bff9519bc4afaf736a5adb
Oliver Dietsche <oliver.dietsche@namics.com>
2020-12-16 16:30:06 +0100

    feature: basic homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import { Container, Grid, Typography } from '@material-ui/core';
-import React from 'react';
+import Image from 'next/image';
+import { CONTENT_HEIGHT } from '../theme';
 
 export default function HomePage() {
 	return (
-		<Container maxWidth="md">
-			<Grid container spacing={2}>
+		<Container
+			css={css`
+				height: ${CONTENT_HEIGHT};
+				display: flex;
+				align-items: center;
+			`}
+			maxWidth="md"
+		>
+			<Grid container spacing={2} justify="center">
 				<Grid item xs={12}>
-					<Typography>HOME</Typography>
+					<Typography variant="h2" color="textPrimary" align="center">
+						Welcome to Bullet Todo!
+					</Typography>
 				</Grid>
+				<Image src="/icon-512x512.png" alt="alt text" width="256" height="256" />
 			</Grid>
 		</Container>
 	);
