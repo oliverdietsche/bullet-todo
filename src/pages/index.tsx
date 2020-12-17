92a75538f18486cf39764c06a5d1f2a4ed507fa6
Oliver Dietsche <oliver.dietsche@namics.com>
2020-12-17 08:41:48 +0100

    feature: homepage responsiveness update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,31 @@
 import { css, jsx } from '@emotion/core';
 import { Container, Grid, Typography } from '@material-ui/core';
 import Image from 'next/image';
-import { CONTENT_HEIGHT } from '../theme';
 
 export default function HomePage() {
 	return (
 		<Container
 			css={css`
-				height: ${CONTENT_HEIGHT};
-				display: flex;
-				align-items: center;
+				margin-top: 50px;
 			`}
 			maxWidth="md"
 		>
-			<Grid container spacing={2} justify="center">
+			<Grid container spacing={5} justify="center">
 				<Grid item xs={12}>
 					<Typography variant="h2" color="textPrimary" align="center">
 						Welcome to Bullet Todo!
 					</Typography>
 				</Grid>
-				<Image src="/icon-512x512.png" alt="alt text" width="256" height="256" />
+				<Grid
+					item
+					xs={12}
+					css={css`
+						display: flex;
+						justify-content: center;
+					`}
+				>
+					<Image src="/icon-512x512.png" alt="alt text" width="256" height="256" />
+				</Grid>
 			</Grid>
 		</Container>
 	);
